2affad3072419556237bf97d3ed384225fad3ee3
hackplayerz <dev.alter0@gmail.com>
2019-03-19 15:10:56 +0900

    03-19

---
+++ Orb-Master/Assets/01.Script/Orb/OrbController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class OrbController : MonoBehaviour
+{
+
+    [SerializeField] private float _distance;
+    [SerializeField][Range(0,5f)] private float _speed;
+    [SerializeField] private Transform _targetTrasnform;
+
+    private Vector3 _rotation;
+    private float _radian = 0;
+
+    void Start () {
+        StartCoroutine (MoveCircle ());
+    }
+
+    IEnumerator MoveCircle () {
+        while (true) {
+            yield return new WaitForEndOfFrame ();
+            transform.position = new Vector3 (Mathf.Sin (_radian) * _distance, Mathf.Cos (_radian) * _distance, 0);
+            _radian += _speed * Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ Orb-Master/Assets/01.Script/Player/MoveController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    
+    #region SerializedField
+
+    [Range(1,10f)][SerializeField] private float _speed;
+    
+    #endregion
+
+    #region MainProcessor
+
+    private void Update()
+    {
+        Move();
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Move Object
+    /// </summary>
+    void Move()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        
+        transform.position += new Vector3(inputX,inputY,0) * _speed * Time.deltaTime; // Translate Object
+        if (inputX > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (inputX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+}
